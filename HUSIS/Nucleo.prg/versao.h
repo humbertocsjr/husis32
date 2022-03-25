1020d1937dee067e6374884957c3ba729848d457
humbertocsjr <humberto.costa@me.com>
2022-03-25 12:58:50 -0300

    Compilação de Teste

--- HUSIS/Nucleo.prg/versao.h
+++ HUSIS/Nucleo.prg/versao.h
@@ -2,5 +2,5 @@
 #define COMPILACAO 0
 const uint16_t husis_versao = 22;
 const uint16_t husis_sub_versao = 03;
-const uint16_t husis_revisao = 24 * 10 + COMPILACAO;
-const txt_t husis_nome = "HUSIS v22.03 R240";
+const uint16_t husis_revisao = 25 * 10 + COMPILACAO;
+const txt_t husis_nome = "HUSIS v22.03 R250";
